11fe36280f35d557c0951385b2eb688800d3d3d6
hammy <jorge@domingues.tech>
2024-05-11 15:04:12 +0100

    added tp5 files

---
+++ MiniTP-5/TVseries.cpp
@@ -0,0 +1,93 @@
+#include "TVseries.hpp"
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <stack>
+
+TVSeriesAPP::TVSeriesAPP() {
+
+}
+   
+TVSeriesAPP::~TVSeriesAPP() {
+    
+}
+
+
+void TVSeriesAPP::addTitleBasics(const TitleBasics& title) {
+    
+ }
+void TVSeriesAPP::addTitleEpisodes(const TitleEpisode& episode) {
+  
+}
+
+
+void TVSeriesAPP::addTitlePrincipal(const TitlePrincipals& principal) {
+
+    
+}
+
+
+
+//Pergunta 1:
+
+vector<string> TVSeriesAPP::getUniquePrincipals(const string& seriesTconst ) const {
+    
+    
+
+    return {};
+}
+
+//PERGUNTA 2:
+
+
+
+string TVSeriesAPP::getMostSeriesGenre() const {
+   
+
+    return "";
+}
+
+
+//PERGUNTA 3: 
+
+string TVSeriesAPP::getPrincipalFromCharacter(const string& character) const {
+
+    return "";
+}
+
+
+
+//PERGUNTA 4
+
+vector<string> TVSeriesAPP::principalsWithMultipleCategories(const string& seriesTconst ) const {
+
+  return {};
+}
+ 
+//PERGUNTA 5:
+
+int TVSeriesAPP::principalInMultipleGenres(vector<string> vGenres) {
+
+
+return -1;
+}
+
+
+//PERGUNTA 6: 
+vector<string> TVSeriesAPP::principalsInAllEpisodes(const string& seriesTconst) const {
+
+    return {};
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
